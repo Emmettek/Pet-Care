e313852a50831505a76b3093e2cd94a7b30bf779
Himil192 <himilprajapati2611@gmail.com>
2025-04-11 22:45:39 +0530

    create a [reusable navbar],[globale theme dark theme ] , home,about,contact us page

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,11 +1,38 @@
-import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './component/Navbar';
+import Home from './page/Home';
+import About from './page/About';
+import ContactUs from './page/ContactUs';
 
 function App() {
+  const navLinks = [
+    { name: 'Home', href: '/' },
+    { name: 'About', href: '/about' },
+    { name: 'Contact Us', href: '/contact-us' },
+
+  ];
 
   return (
     <>
+      <Router>
+
+        <Navbar links={navLinks} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact-us' element={<ContactUs />} />
+
+        </Routes>
+      </Router>
+
+
+
+
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
